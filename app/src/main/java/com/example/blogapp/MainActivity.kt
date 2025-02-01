54aaa806d7e5b07532ecedcb76bde31740ef1816
aayushma12 <aayushmaacharya90@gmail.com>
2025-02-01 15:39:57 +0545

    Recycler View

--- app/src/main/java/com/example/blogapp/MainActivity.kt
+++ app/src/main/java/com/example/blogapp/MainActivity.kt
@@ -2,24 +2,98 @@ package com.example.blogapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.example.blogapp.Model.BlogItemModel
+import com.example.blogapp.adapter.BlogAdapter
 import com.example.blogapp.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 
 class MainActivity : AppCompatActivity() {
     private val binding : ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+    private lateinit var databaseReference : DatabaseReference
+    private val blogItems= mutableListOf<BlogItemModel>()
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        auth = FirebaseAuth.getInstance()
+        databaseReference=FirebaseDatabase.getInstance().reference.child("blogs")
+
+        val userId = auth.currentUser?.uid
+//set user profile
+        if (userId !=null){
+            loadUserProfileImage(userId)
+        }
+
+        //set blog post into recycler view
+
+
+        //Initialize the recycler view
+        val recyclerView = binding.blogRecyclerView
+        val blogAdapter = BlogAdapter(blogItems)
+        recyclerView.adapter = blogAdapter
+        recyclerView.layoutManager=LinearLayoutManager(this)
+
+        //fetch data from firebase database
+        databaseReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                blogItems.clear()
+                for (snapshot in snapshot.children){
+                    val blogItem = snapshot.getValue(BlogItemModel::class.java)
+                    if (blogItem!=null){
+                        blogItems.add(blogItem)
+                    }
+                }
+
+
+
+                //Notify the adapter that the data has changed
+                blogAdapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@MainActivity, "Blog loading failed", Toast.LENGTH_SHORT).show()
+            }
+        })
         binding.floatingAddArticleButton.setOnClickListener {
             startActivity(Intent(this,AddArticleActivity::class.java))
         }
 
 
+
+    }
+
+    private fun loadUserProfileImage(userId: String) {
+        val userReference = FirebaseDatabase.getInstance().reference.child("users").child(userId)
+
+        userReference.child("profileImage").addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val profileImageUrl=snapshot.getValue(String::class.java)
+
+                if(profileImageUrl !=null){
+                    Glide.with(this@MainActivity)
+                        .load(profileImageUrl)
+                        .into(binding.profileImage)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@MainActivity, "Error loading profile image", Toast.LENGTH_SHORT).show()
+            }
+        } )
 
     }
 
