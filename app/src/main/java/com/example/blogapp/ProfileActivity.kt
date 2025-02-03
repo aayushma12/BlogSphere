62b5d9c2b1f4ffdfaf722c7e447164c9b0ee56a0
aayushma12 <aayushmaacharya90@gmail.com>
2025-02-03 14:49:22 +0545

    add blog

--- app/src/main/java/com/example/blogapp/ProfileActivity.kt
+++ app/src/main/java/com/example/blogapp/ProfileActivity.kt
@@ -1,10 +1,12 @@
 package com.example.blogapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.blogapp.databinding.ActivityProfileBinding
+import com.example.blogapp.register.WelcomeActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -23,6 +25,20 @@ class ProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(binding.root)
+
+        // to go add article page
+        binding.addNewBlogButton.setOnClickListener {
+            startActivity(Intent(this,SavedArticlesActivity::class.java))
+        }
+        //to logout
+        // to go save article page
+        binding.logoutButton.setOnClickListener {
+            auth.signOut()
+
+            //navigate
+            startActivity(Intent(this,WelcomeActivity::class.java))
+            finish()
+        }
 
         // Initialize Firebase
         auth = FirebaseAuth.getInstance()
